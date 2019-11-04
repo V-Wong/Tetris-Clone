886ebf87fd8edd9074d234010e8fec06350b1c00
Vincent Wong <TheBrutux168@gmail.com>
2019-11-04 21:29:01 +1100

    Implement basic collision detection

--- main.py
+++ main.py
@@ -26,17 +26,23 @@ class Game:
                 
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_RIGHT:
+                        if self.calculate_collision(1, 0): 
+                            break
                         self.x += 1
                     elif event.key == pygame.K_LEFT:
+                        if self.calculate_collision(-1, 0): 
+                            break
                         self.x -= 1
                     elif event.key == pygame.K_DOWN:
+                        if self.calculate_collision(0, 1): 
+                            break
                         self.y += 1
                     self.update_screen()
 
-            time.sleep(0.1)
-            self.y += 1
-            self.update_screen()
-            
+            if not self.calculate_collision(0, 2): 
+                time.sleep(0.05)
+                self.y += 1
+                self.update_screen()
 
     def update_screen(self):
         self.screen.fill((0,0,0))
@@ -46,6 +52,10 @@ class Game:
     def draw_block(self):
         pygame.draw.rect(self.screen, (255, 255, 255), 
                         (self.x * 50, self.y * 50, 50, 50))
+
+    def calculate_collision(self, dx, dy):
+        return (not (0 <= self.x + dx < self.grid_width)
+                or not (0 <= self.y + dy <= self.grid_height))
 
 
 if __name__ == "__main__":
